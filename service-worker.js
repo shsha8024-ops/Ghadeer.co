c4c146ef2627c21c9dab5d05ef4cac6bc3e8576b
shsha <shsha8024@gmail.com>
2025-10-22 03:47:16 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
+
+
 const CACHE='ghadeer-login-cache-v1';
 const ASSETS=['./','./index.html','./assets/css/theme.css','./assets/js/crypto.js','./assets/js/app.js','./assets/icons/truck.png','./assets/icons/brand-card.jpg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
